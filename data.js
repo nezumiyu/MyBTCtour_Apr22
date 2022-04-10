d0c7c9a980c224ea1034ea31f981014ff78803d0
nezumiyu <102938885+nezumiyu@users.noreply.github.com>
2022-04-10 23:07:45 +0800

    changed starting screen of tour
    change starting of tour to start from Law stone instead of library

--- data.js
+++ data.js
@@ -1,58 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-c-j-koh-law-library",
-      "name": "C J Koh Law Library",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": 0.04194539568291944,
-        "pitch": 0.3912924356000911,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.4728855222740123,
-          "pitch": 0.21226118761009616,
-          "rotation": 12.566370614359176,
-          "target": "1-stone-plate-of-nus-law"
-        },
-        {
-          "yaw": 3.091731005131626,
-          "pitch": 0.1418711488921378,
-          "rotation": 5.497787143782138,
-          "target": "2-the-summit-canteen"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.0837705858504183,
-          "pitch": 0.17916369534045984,
-          "title": "C J Koh Law Library",
-          "text": "<div>For info and opening hours, please check this link:&nbsp;</div><div>https://nus.edu.sg/nuslibraries/spaces/our-libraries/c-j-koh-law-library<br></div><div><br></div>"
-        }
-      ]
-    },
-    {
-      "id": "1-stone-plate-of-nus-law",
+      "id": "0-stone-plate-of-nus-law",
       "name": "Stone Plate of NUS Law",
       "levels": [
         {
@@ -84,7 +33,7 @@ var APP_DATA = {
           "yaw": -1.854190530399224,
           "pitch": 0.190107517948519,
           "rotation": 5.497787143782138,
-          "target": "0-c-j-koh-law-library"
+          "target": "1-c-j-koh-law-library"
         },
         {
           "yaw": 1.8811489555183503,
@@ -99,6 +48,57 @@ var APP_DATA = {
           "pitch": 0.22619482904042876,
           "title": "Faculty of Law",
           "text": "Entrance to the Faculty of Law office"
+        }
+      ]
+    },
+    {
+      "id": "1-c-j-koh-law-library",
+      "name": "C J Koh Law Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.04194539568291944,
+        "pitch": 0.3912924356000911,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4728855222740123,
+          "pitch": 0.21226118761009616,
+          "rotation": 12.566370614359176,
+          "target": "0-stone-plate-of-nus-law"
+        },
+        {
+          "yaw": 3.091731005131626,
+          "pitch": 0.1418711488921378,
+          "rotation": 5.497787143782138,
+          "target": "2-the-summit-canteen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0837705858504183,
+          "pitch": 0.17916369534045984,
+          "title": "C J Koh Law Library",
+          "text": "<div>For info and opening hours, please check this link:&nbsp;</div><div>https://nus.edu.sg/nuslibraries/spaces/our-libraries/c-j-koh-law-library<br></div><div><br></div>"
         }
       ]
     },
@@ -135,13 +135,13 @@ var APP_DATA = {
           "yaw": 1.1800315973545192,
           "pitch": 0.3534319828089316,
           "rotation": 0,
-          "target": "1-stone-plate-of-nus-law"
+          "target": "0-stone-plate-of-nus-law"
         },
         {
           "yaw": -2.646561891059882,
           "pitch": 0.34131477182236836,
           "rotation": 7.0685834705770345,
-          "target": "0-c-j-koh-law-library"
+          "target": "1-c-j-koh-law-library"
         }
       ],
       "infoHotspots": [
