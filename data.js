b7f0a8c5c0f3014b90665a2921f3bc52689c9341
nezumiyu <102938885+nezumiyu@users.noreply.github.com>
2022-04-10 22:42:54 +0800

    first commit
    first upload of files into folder

---
+++ data.js
@@ -0,0 +1,164 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-c-j-koh-law-library",
+      "name": "C J Koh Law Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.04194539568291944,
+        "pitch": 0.3912924356000911,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4728855222740123,
+          "pitch": 0.21226118761009616,
+          "rotation": 12.566370614359176,
+          "target": "1-stone-plate-of-nus-law"
+        },
+        {
+          "yaw": 3.091731005131626,
+          "pitch": 0.1418711488921378,
+          "rotation": 5.497787143782138,
+          "target": "2-the-summit-canteen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0837705858504183,
+          "pitch": 0.17916369534045984,
+          "title": "C J Koh Law Library",
+          "text": "<div>For info and opening hours, please check this link:&nbsp;</div><div>https://nus.edu.sg/nuslibraries/spaces/our-libraries/c-j-koh-law-library<br></div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-stone-plate-of-nus-law",
+      "name": "Stone Plate of NUS Law",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.0139325852182424,
+        "pitch": 0.20822654010279784,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.854190530399224,
+          "pitch": 0.190107517948519,
+          "rotation": 5.497787143782138,
+          "target": "0-c-j-koh-law-library"
+        },
+        {
+          "yaw": 1.8811489555183503,
+          "pitch": 0.1330119500446898,
+          "rotation": 7.0685834705770345,
+          "target": "2-the-summit-canteen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0758459128405224,
+          "pitch": 0.22619482904042876,
+          "title": "Faculty of Law",
+          "text": "Entrance to the Faculty of Law office"
+        }
+      ]
+    },
+    {
+      "id": "2-the-summit-canteen",
+      "name": "The Summit Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.14425029527835775,
+        "pitch": 0.22642109215061978,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1800315973545192,
+          "pitch": 0.3534319828089316,
+          "rotation": 0,
+          "target": "1-stone-plate-of-nus-law"
+        },
+        {
+          "yaw": -2.646561891059882,
+          "pitch": 0.34131477182236836,
+          "rotation": 7.0685834705770345,
+          "target": "0-c-j-koh-law-library"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.31480239746213456,
+          "pitch": 0.4811457997944011,
+          "title": "The Summit Canteen",
+          "text": "The one and only canteen at the Bukit Timah Campus"
+        }
+      ]
+    }
+  ],
+  "name": "NUS BTC campus virtual tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
